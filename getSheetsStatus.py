b00e783a1004288b7ca9a51f0dcbeb4da156fade
Mark Kortekaas <mark@kortekaas.com>
2019-08-12 11:31:51 -0400

    added-google-sheets-pull

---
+++ getSheetsStatus.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START sheets_quickstart]
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '13u6jXHs5JQTGIUQP-SeFxQo7_cb5y7f-RGzRtKa8NXo'
+SAMPLE_RANGE_NAME = 'Sheet1!A1:A1'
+PICKLE_FILE = '/home/pi/token.pickle'
+CRED_FILE = '/home/pi/credentials.json'
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(PICKLE_FILE):
+        with open(PICKLE_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CRED_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            status = row[0]
+            if status == "Enabled":
+                print( "turning on sprinkler")
+                os.system('/home/pi/git/RainBypass/setSprinkler.pl -e')
+            elif status == "Disabled":
+                print( "turn off")
+                os.system('/home/pi/git/RainBypass/setSprinkler.pl -d')
+            else:
+                print( "something different...")
+                
+            
+
+#            print('%s' % (row[0]))
+
+if __name__ == '__main__':
+    main()
+# [END sheets_quickstart]
